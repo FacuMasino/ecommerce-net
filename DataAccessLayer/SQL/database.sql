3a9f91cdeb9e960617118ac06f5ace12aa80f652
Facundo Masino <facujmasino@gmail.com>
2024-06-02 16:44:06 -0300

    Corrijo formateo erroneo db

--- DataAccessLayer/SQL/database.sql
+++ DataAccessLayer/SQL/database.sql
@@ -1,175 +1,191 @@
-create
-database ecommerce
-collate Latin1_General_100_CI_AS_SC_UTF8;
+CREATE
+DATABASE ecommerce
+COLLATE Latin1_General_100_CI_AS_SC_UTF8;
 
-go use ecommerce go
+GO
+USE ecommerce 
+GO
 
 ------------
 -- BRANDS --
 ------------
 
-create table
-	Brands (
-		BrandId int primary key identity(1, 1) not null,
-		BrandName varchar(30) unique not null
-	) go
+CREATE TABLE Brands
+(
+	BrandId int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	BrandName varchar(30) UNIQUE NOT NULL
+) 
+GO
 
 ----------------
 -- CATEGORIES --
 ----------------
 
-create table
-	Categories (
-		CategoryId int primary key identity(1, 1) not null,
-		CategoryName varchar(30) unique not null
-	) go
+CREATE TABLE Categories
+(
+	CategoryId int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	CategoryName varchar(30) UNIQUE NOT NULL
+) 
+GO
 
 --------------
 -- PRODUCTS --
 --------------
 
-create table
-	Products (
-		ProductId int primary key identity(1, 1) not null,
-		Code varchar(50) not null,
-		ProductName varchar(50) not null,
-		ProductDescription varchar(150) null,
-		Price decimal(15, 2) not null,
-		BrandId int foreign key references Brands (BrandId) null,
-		CategoryId int foreign key references Categories (CategoryId) null
-	) go
+CREATE TABLE Products
+(
+	ProductId int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	Code varchar(50) NOT NULL,
+	ProductName varchar(50) NOT NULL,
+	ProductDescription varchar(300) NULL,
+	Price decimal(15, 2) NOT NULL,
+	BrandId int FOREIGN KEY REFERENCES Brands (BrandId) NULL,
+	CategoryId int FOREIGN KEY REFERENCES Categories (CategoryId) NULL
+) 
+GO
 
 ------------
 -- IMAGES --
 ------------
 
-create table
-	Images (
-		ImageId int primary key identity(1, 1) not null,
-		ImageUrl varchar(300) unique not null,
-		ProductId int foreign key references Products (ProductId) not null
-	) go
+CREATE TABLE Images
+(
+	ImageId int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	ImageUrl varchar(300) UNIQUE NOT NULL,
+	ProductId int FOREIGN KEY REFERENCES Products (ProductId) NOT NULL
+) 
+GO
 
 ---------------
 -- COUNTRIES --
 ---------------
 
-create table
-	Countries (
-		CountryId tinyint primary key identity(1, 1) not null,
-		CountryName varchar(30) unique not null
-	) go
+CREATE TABLE Countries
+(
+	CountryId tinyint PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	CountryName varchar(30) UNIQUE NOT NULL
+) 
+GO
 
 ---------------
 -- PROVINCES --
 ---------------
 
-create table
-	Provinces (
-		ProvinceId smallint primary key identity(1, 1) not null,
-		ProvinceName varchar(30) not null,
-		CountryId tinyint foreign key references Countries (CountryId) not null,
-		constraint UC_Province unique (ProvinceName, CountryId)
-	) go
+CREATE TABLE Provinces
+(
+	ProvinceId smallint PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	ProvinceName varchar(30) NOT NULL,
+	CountryId tinyint FOREIGN KEY REFERENCES Countries (CountryId) NOT NULL,
+	CONSTRAINT UC_Province UNIQUE (ProvinceName, CountryId)
+) 
+GO
 
 ------------
 -- CITIES --
 ------------
 
-create table
-	Cities (
-		CityId smallint primary key identity(1, 1) not null,
-		CityName varchar(30) not null,
-		ZipCode varchar(30) null,
-		ProvinceId smallint foreign key references Provinces (ProvinceId) not null,
-		constraint UC_City unique (CityName, ProvinceId)
-	) go
+CREATE TABLE Cities
+(
+	CityId smallint PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	CityName varchar(30) NOT NULL,
+	ZipCode varchar(30) NULL,
+	ProvinceId smallint FOREIGN KEY REFERENCES Provinces (ProvinceId) NOT NULL,
+	CONSTRAINT UC_City UNIQUE (CityName, ProvinceId)
+) 
+GO
 
 --------------
 -- ADRESSES --
 --------------
 
-create table
-	Adresses (
-		AdressId int primary key identity(1, 1) not null,
-		StreetName varchar(30) not null,
-		StreetNumber varchar(10) not null,
-		Flat varchar(30) null,
-		Details varchar(300) null,
-		CityId smallint foreign key references Cities (CityId) not null,
-		constraint UC_Adress unique (StreetName, StreetNumber, CityId)
-	) go
+CREATE TABLE Adresses
+(
+	AdressId int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	StreetName varchar(30) NOT NULL,
+	StreetNumber varchar(10) NOT NULL,
+	Flat varchar(30) NULL,
+	Details varchar(300) NULL,
+	CityId smallint FOREIGN KEY REFERENCES Cities (CityId) NOT NULL,
+	CONSTRAINT UC_Adress UNIQUE (StreetName, StreetNumber, CityId)
+) 
+GO
 
 ------------
 -- PEOPLE --
 ------------
 
-create table
-	People (
-		PersonId int primary key identity(1, 1) not null,
-		PersonStatus bit not null default (1),
-		PersonFirstName varchar(30) not null,
-		PersonLastName varchar(30) not null,
-		TaxCode varchar(30) null,
-		Phone varchar(30) null,
-		Email varchar(30) null,
-		Birth date null,
-		AdressId int foreign key references Adresses (AdressId) null,
-	) go
+CREATE TABLE People
+(
+	PersonId int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	PersonStatus bit NOT NULL DEFAULT (1),
+	PersonFirstName varchar(30) NOT NULL,
+	PersonLastName varchar(30) NOT NULL,
+	TaxCode varchar(30) NULL,
+	Phone varchar(30) NULL,
+	Email varchar(30) NULL,
+	Birth date NULL,
+	AdressId int FOREIGN KEY REFERENCES Adresses (AdressId) NULL,
+) 
+GO
 
 -----------
 -- ROLES --
 -----------
 
-create table
-	Roles (
-		RoleId tinyint primary key identity(1, 1) not null,
-		RoleName varchar(50) not null
-	) go
+CREATE TABLE Roles
+(
+	RoleId tinyint PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	RoleName varchar(50) NOT NULL
+) 
+GO
 
 -----------
 -- USERS --
 -----------
 
-create table
-	Users (
-		UserId int primary key identity(1, 1) not null,
-		UserName varchar(30) not null,
-		UserPassword varchar(30) not null,
-		RoleId tinyint foreign key references Roles (RoleId) not null,
-		PersonId int foreign key references People (PersonId) not null
-	) go
+CREATE TABLE Users
+(
+	UserId int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	UserName varchar(30) NOT NULL,
+	UserPassword varchar(30) NOT NULL,
+	RoleId tinyint FOREIGN KEY REFERENCES Roles (RoleId) NOT NULL,
+	PersonId int FOREIGN KEY REFERENCES People (PersonId) NOT NULL
+) 
+GO
 
 -------------------
 -- ORDERSTATUSES --
 -------------------
 
-create table
-	OrderStatuses (
-		OrderStatusId int primary key identity(1, 1) not null,
-		OrderStatusName varchar(30) not null
-	) go
+CREATE TABLE OrderStatuses
+(
+	OrderStatusId int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	OrderStatusName varchar(30) NOT NULL
+) 
+GO
 
 ------------
 -- ORDERS --
 ------------
 
-create table
-	Orders (
-		OrderId int primary key identity(1, 1) not null,
-		OrderDate datetime default (getdate()) not null,
-		AdressId int foreign key references Adresses (AdressId) null,
-		OrderStatusId int foreign key references OrderStatuses (OrderStatusId) not null,
-		UserId int foreign key references Users (UserId) not null
-	) go
+CREATE TABLE Orders
+(
+	OrderId int PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	OrderDate datetime DEFAULT (getdate()) NOT NULL,
+	AdressId int FOREIGN KEY REFERENCES Adresses (AdressId) NULL,
+	OrderStatusId int FOREIGN KEY REFERENCES OrderStatuses (OrderStatusId) NOT NULL,
+	UserId int FOREIGN KEY REFERENCES Users (UserId) NOT NULL
+) 
+GO
 
 -----------------------
 -- ORDERS X PRODUCTS --
 -----------------------
 
-create table
-	OrdersXProducts (
-		OrderId int foreign key references Orders (OrderId) not null,
-		ProductId int foreign key references Products (ProductId) not null,
-		Amount int default (1) not null
-	) go
+CREATE TABLE OrdersXProducts
+(
+	OrderId int FOREIGN KEY REFERENCES Orders (OrderId) NOT NULL,
+	ProductId int FOREIGN KEY REFERENCES Products (ProductId) NOT NULL,
+	Amount int DEFAULT (1) NOT NULL
+) 
+GO
